268472b7d96dc8332d862e000131077419e8dbb4
Fuad Shirinli <fuad.shirinli@rentsoft.de>
2021-08-25 16:25:37 +0200

    bugs fixed

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -268,7 +268,7 @@
         </div>
     </div>
 
-    <div class="modal fade bd-example-modal-lg" id="addUser" tabindex="-1" role="dialog"
+    <div class="modal fade show bd-example-modal-lg" id="addUser" tabindex="-1" role="dialog"
          aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
